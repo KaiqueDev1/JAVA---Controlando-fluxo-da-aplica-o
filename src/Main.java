aaa235a6bf3f8b28da8e5989adf7e63865b8b514
kaiqueDev1 <kakamelo141205@gmail.com>
2025-07-03 20:19:04 -0300

    Initial project

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        if(anoDeLancamento >= 2022) {
+            System.out.println("Lançamento que os clientes estão curtindo!");
+        }
+
+    }
+}
