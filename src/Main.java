b2e80340891c75c1aaa7d74d8565a3fe45f00674
kaiqueDev1 <kakamelo141205@gmail.com>
2025-07-04 19:43:19 -0300

    FEAT: Aprendendo sobre if/else e switch case em java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int anoDeLancamento = 2022;
+        int anoDeLancamento = 2021;
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
 
         if(anoDeLancamento >= 2022) {
             System.out.println("Lançamento que os clientes estão curtindo!");
+        } else {
+            System.out.println("Filme retrô que vale a pena assistir");
         }
+
+
 
     }
 }
